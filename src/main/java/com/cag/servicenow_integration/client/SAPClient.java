3bae07f570eb61eaab953d23dcd27dbd50d75bb7
NgocHN <ngochn2461@gmail.com>
2025-11-12 14:46:37 +0700

    Change SAP Client and Service

--- src/main/java/com/cag/servicenow_integration/client/SAPClient.java
+++ src/main/java/com/cag/servicenow_integration/client/SAPClient.java
@@ -1,21 +1,12 @@
 package com.cag.servicenow_integration.client;
 
-import com.cag.servicenow_integration.config.SAPApiProperties;
-import com.cag.servicenow_integration.constants.Constants;
-import com.cag.servicenow_integration.dto.sap.OnboardingCandidateInfoDTO;
 import com.cag.servicenow_integration.enums.ResponseCode;
-import com.cag.servicenow_integration.exception.DomainException;
 import com.cag.servicenow_integration.exception.InternalServerException;
 import com.cag.servicenow_integration.exception.NotFoundException;
-import com.cag.servicenow_integration.response.BaseResponse;
-import com.cag.servicenow_integration.response.PaginatedResponse;
-import com.cag.servicenow_integration.response.sap.PageMetaDTO;
-import com.cag.servicenow_integration.response.sap.ResponseDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
@@ -23,146 +14,68 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @Component
 public class SAPClient {
-    private final SAPApiProperties sapApiProperties;
-
     private final WebClient webClient;
 
-    public SAPClient(SAPApiProperties sapApiProperties, WebClient webClient) {
-        this.sapApiProperties = sapApiProperties;
+    public SAPClient(WebClient webClient) {
         this.webClient = webClient;
     }
 
-    public BaseResponse getEmployeeProfiles(int skip, int limit) {
-        String url = sapApiProperties.getBaseUrl() + sapApiProperties.getOnboardingCandidateEndpoint() + "?skip=" + skip + "&limit=" + limit;
-        try {
-            log.debug("Requesting SAP profiles with url: " + url);
-            // Expect Domain response
-            Mono<ResponseDTO<PageMetaDTO<OnboardingCandidateInfoDTO>>> response = webClient.get()
-                    .uri(url)
-                    .accept(MediaType.APPLICATION_JSON)
-                    .headers(headers -> {
-                        if (StringUtils.hasText(sapApiProperties.getAuthToken())) {
-                            headers.setBearerAuth(sapApiProperties.getAuthToken());
-                        }
-                    })
-                    .retrieve()
-                    .bodyToMono(new ParameterizedTypeReference<ResponseDTO<PageMetaDTO<OnboardingCandidateInfoDTO>>>() {
-                    });
-
-            ResponseDTO<PageMetaDTO<OnboardingCandidateInfoDTO>> sapResponse = response.block();
-
-            if (sapResponse == null) throw new InternalServerException(
-                    Constants.SAP_RESPONSE_NULL_MSG,
-                    ResponseCode.ERROR.getCode(),
-                    String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value())
-            );
-
-            if (sapResponse.getCode() != HttpStatus.OK.value()) {
-                return BaseResponse.builder()
-                        .responseCode(sapResponse.getStatus())
-                        .code(String.valueOf(sapResponse.getCode()))
-                        .message(sapResponse.getMessage())
-                        .build();
-            }
-
-            if (sapResponse.getData().getContent().isEmpty()) {
-                PaginatedResponse<OnboardingCandidateInfoDTO> paginatedResponse = PaginatedResponse.<OnboardingCandidateInfoDTO>builder()
-                        .totalRecords(sapResponse.getData().getTotalElements())
-                        .skip(sapResponse.getData().getNumber())
-                        .limit(sapResponse.getData().getSize())
-                        .hasNext(!sapResponse.getData().isLast())
-                        .nextSkip(sapResponse.getData().getNumber() + sapResponse.getData().getSize())
-                        .build();
-//                throw new NotFoundException(Constants.NOT_FOUND_MSG + paginatedResponse, ResponseCode.NOT_FOUND.getCode(), String.valueOf(HttpStatus.NOT_FOUND.value()));
-            }
-
-            PaginatedResponse<OnboardingCandidateInfoDTO> paginatedResponse = PaginatedResponse.<OnboardingCandidateInfoDTO>builder()
-                    .data(sapResponse.getData().getContent())
-                    .totalRecords(sapResponse.getData().getTotalElements())
-                    .skip(sapResponse.getData().getNumber())
-                    .limit(sapResponse.getData().getSize())
-                    .hasNext(!sapResponse.getData().isLast())
-                    .nextSkip(sapResponse.getData().getNumber() + sapResponse.getData().getSize())
-                    .build();
-
-            return BaseResponse.builder()
-                    .responseCode(ResponseCode.SUCCESS.getCode())
-                    .code(String.valueOf(HttpStatus.OK.value()))
-                    .message(Constants.SUCCESS_MSG)
-                    .data(paginatedResponse)
-                    .build();
-        } catch (WebClientResponseException.NotFound ex) {
-            log.warn("Onboarding Candidate Info list is empty: {}", ex.getMessage());
-            throw new NotFoundException(ex.getMessage(), ResponseCode.NOT_FOUND.getCode(), String.valueOf(HttpStatus.NOT_FOUND.value()));
-        } catch (WebClientResponseException ex) {
-            log.error("Unexpected error calling SAP: {}", ex.getMessage(), ex);
-            throw new InternalServerException(ex.getMessage(), ResponseCode.ERROR.getCode(), String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()));
-        } catch (DomainException ex) {
-            log.error("Middleware error: {}", ex.getMessage(), ex);
-            throw ex;
-        }
+    public <T> T getRequest(String uri, ParameterizedTypeReference<T> reference) {
+        return executeRequest(webClient.get(), uri, reference);
     }
 
-    public BaseResponse getEmployeeProfile(String id) {
-        String url = buildUrl(sapApiProperties.getBaseUrl(), sapApiProperties.getOnboardingCandidateEndpoint(), id);
-
-        try {
-            log.debug("Requesting a SAP profile url: " + url);
-            Mono<ResponseDTO<OnboardingCandidateInfoDTO>> response = webClient.get()
-                    .uri(url)
-                    .accept(MediaType.APPLICATION_JSON)
-                    .headers(headers -> {
-                        if (StringUtils.hasText(sapApiProperties.getAuthToken())) {
-                            headers.setBearerAuth(sapApiProperties.getAuthToken());
-                        }
-                    })
-                    .retrieve()
-                    .bodyToMono(new ParameterizedTypeReference<ResponseDTO<OnboardingCandidateInfoDTO>>() {
-                    });
-
-            ResponseDTO<OnboardingCandidateInfoDTO> sapResponse = response.block();
-            if (sapResponse == null) {
-                throw new InternalServerException(
-                        Constants.SAP_RESPONSE_NULL_MSG,
-                        ResponseCode.ERROR.getCode(),
-                        String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value())
-                );
-            }
-
-            if (sapResponse.getCode() != HttpStatus.OK.value()) {
-                return BaseResponse.builder()
-                        .responseCode(sapResponse.getStatus())
-                        .code(String.valueOf(sapResponse.getCode()))
-                        .message(sapResponse.getMessage())
-                        .build();
-            }
-
-            return BaseResponse.builder()
-                    .responseCode(0)
-                    .code(String.valueOf(HttpStatus.OK.value()))
-                    .message(Constants.SUCCESS_MSG)
-                    .data(sapResponse.getData())
-                    .build();
-        } catch (WebClientResponseException.NotFound ex) {
-            log.warn("Onboarding Candidate Info not found for id {}: {}", id, ex.getMessage());
-            throw new NotFoundException(ex.getMessage(), ResponseCode.NOT_FOUND.getCode(), String.valueOf(HttpStatus.NOT_FOUND.value()));
-        } catch (WebClientResponseException ex) {
-            log.error("Unexpected error calling SAP: {}", ex.getMessage(), ex);
-            throw new InternalServerException(ex.getMessage(), ResponseCode.ERROR.getCode(), String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()));
-        } catch (DomainException ex) {
-            log.error(Constants.MIDDLEWARE_ERROR_MSG, ex.getMessage(), ex);
-            throw ex;
-        }
+    public <T> T putRequest(String uri, Object body, ParameterizedTypeReference<T> reference) {
+        return executeRequest(webClient.put(), uri, body, reference);
     }
 
-    private String buildUrl(String baseUrl, String endpoint, String id) {
-        if (baseUrl == null) baseUrl = "";
-        if (endpoint == null) endpoint = "";
-        String normalizedBase = baseUrl.endsWith("/") ? baseUrl.substring(0, baseUrl.length() - 1) : baseUrl;
-        String normalizedEndpoint = endpoint.startsWith("/") ? endpoint : "/" + endpoint;
-        if (!normalizedEndpoint.endsWith("/")) {
-            normalizedEndpoint = normalizedEndpoint + "/";
+    private <T> T executeRequest(
+            WebClient.RequestBodyUriSpec urlSpec,
+            String uri,
+            Object bodyValue,
+            ParameterizedTypeReference<T> reference
+    ) {
+        WebClient.RequestHeadersSpec<?> responseSpec = urlSpec
+                .uri(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(bodyValue);
+        return returnResponseBody(responseSpec, reference);
+    }
+
+    private <T> T executeRequest(
+            WebClient.RequestHeadersUriSpec<?> urlSpec,
+            String uri,
+            ParameterizedTypeReference<T> reference
+    ) {
+        WebClient.RequestHeadersSpec<?> responseSpec = urlSpec
+                .uri(uri);
+        return returnResponseBody(responseSpec, reference);
+    }
+
+    private <T> T returnResponseBody(
+            WebClient.RequestHeadersSpec<?> responseSpec,
+            ParameterizedTypeReference<T> reference
+    ) {
+        return responseSpec
+                .retrieve()
+                .bodyToMono(reference)
+                .onErrorResume(this::handleWebClientErrors)
+                .block();
+    }
+
+    public <T> Mono<T> handleWebClientErrors(Throwable ex) {
+        if (ex instanceof WebClientResponseException.NotFound notFoundEx) {
+            log.warn("Resource not found: {}", notFoundEx.getMessage());
+            return Mono.error(new NotFoundException(
+                    notFoundEx.getMessage(),
+                    ResponseCode.NOT_FOUND.getCode(),
+                    String.valueOf(HttpStatus.NOT_FOUND.value())
+            ));
         }
-        return normalizedBase + normalizedEndpoint + id;
+
+        return Mono.error(new InternalServerException(
+                ex.getMessage(),
+                ResponseCode.ERROR.getCode(),
+                String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value())
+        ));
     }
 }
--- src/main/java/com/cag/servicenow_integration/service/impl/SuccessFactorsServiceImpl.java
+++ src/main/java/com/cag/servicenow_integration/service/impl/SuccessFactorsServiceImpl.java
@@ -1,12 +1,16 @@
 package com.cag.servicenow_integration.service.impl;
 
 import com.cag.servicenow_integration.client.SAPClient;
+import com.cag.servicenow_integration.config.SAPApiProperties;
 import com.cag.servicenow_integration.dto.sap.OnboardingCandidateInfoDTO;
 import com.cag.servicenow_integration.dto.servicenow.EmployeeProfileDTO;
 import com.cag.servicenow_integration.mapper.ServiceNowMapper;
-import com.cag.servicenow_integration.response.BaseResponse;
 import com.cag.servicenow_integration.response.PaginatedResponse;
+import com.cag.servicenow_integration.response.sap.PageMetaDTO;
+import com.cag.servicenow_integration.response.sap.ResponseDTO;
 import com.cag.servicenow_integration.service.SuccessFactorsService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import java.util.Collections;
@@ -14,23 +18,27 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class SuccessFactorsServiceImpl implements SuccessFactorsService {
     private final SAPClient sapClient;
 
     private final ServiceNowMapper serviceNowMapper;
 
-    public SuccessFactorsServiceImpl(SAPClient sapClient, ServiceNowMapper serviceNowMapper) {
+    private final SAPApiProperties sapApiProperties;
+
+    public SuccessFactorsServiceImpl(SAPClient sapClient, ServiceNowMapper serviceNowMapper, SAPApiProperties sapApiProperties) {
         this.sapClient = sapClient;
         this.serviceNowMapper = serviceNowMapper;
+        this.sapApiProperties = sapApiProperties;
     }
 
     @Override
     public EmployeeProfileDTO getEmployeeProfile(String id) {
-        // Call success factors API
-        BaseResponse sapResponse = sapClient.getEmployeeProfile(id);
-        String code = sapResponse.getCode();
+        String url = buildUrl(sapApiProperties.getBaseUrl(), sapApiProperties.getOnboardingCandidateEndpoint(), id);
+        log.debug("Requesting SAP profile with url: ", url);
+        ResponseDTO<OnboardingCandidateInfoDTO> sapResponse = sapClient.getRequest(url, new ParameterizedTypeReference<ResponseDTO<OnboardingCandidateInfoDTO>>() {});
         OnboardingCandidateInfoDTO onboardingCandidateInfoDTO = (OnboardingCandidateInfoDTO) sapResponse.getData();
-        if (String.valueOf(HttpStatus.OK.value()).equals(code) && onboardingCandidateInfoDTO != null) {
+        if (HttpStatus.OK.value() == sapResponse.getCode() && onboardingCandidateInfoDTO != null) {
             return serviceNowMapper.toEmployeeProfileDTO(onboardingCandidateInfoDTO);
         }
         return null;
@@ -38,23 +46,42 @@ public class SuccessFactorsServiceImpl implements SuccessFactorsService {
 
     @Override
     public PaginatedResponse<EmployeeProfileDTO> getEmployeeProfiles(int skip, int limit) {
-        BaseResponse sapResponse = sapClient.getEmployeeProfiles(skip, limit);
-        if (!String.valueOf(HttpStatus.OK.value()).equals(sapResponse.getCode())) return null;
+        String url = sapApiProperties.getBaseUrl() + sapApiProperties.getOnboardingCandidateEndpoint() + "?skip=" + skip + "&limit=" + limit;
+        log.debug("Requesting SAP profiles with url: " + url);
+        ResponseDTO<PageMetaDTO<OnboardingCandidateInfoDTO>> sapResponse = sapClient.getRequest(url, new ParameterizedTypeReference<ResponseDTO<PageMetaDTO<OnboardingCandidateInfoDTO>>>() {});
+        if (HttpStatus.OK.value() != sapResponse.getCode()) return null;
+        PaginatedResponse<OnboardingCandidateInfoDTO> paginatedResponse = PaginatedResponse.<OnboardingCandidateInfoDTO>builder()
+                .data(sapResponse.getData().getContent())
+                .totalRecords(sapResponse.getData().getTotalElements())
+                .skip(sapResponse.getData().getNumber())
+                .limit(sapResponse.getData().getSize())
+                .hasNext(!sapResponse.getData().isLast())
+                .nextSkip(sapResponse.getData().getNumber() + sapResponse.getData().getSize())
+                .build();
 
-        PaginatedResponse<OnboardingCandidateInfoDTO> sapPaginatedResponse = (PaginatedResponse<OnboardingCandidateInfoDTO>) sapResponse.getData();
-
-        List<OnboardingCandidateInfoDTO> content = Optional.ofNullable(sapPaginatedResponse.getData()).orElse(Collections.emptyList());
+        List<OnboardingCandidateInfoDTO> content = Optional.ofNullable(paginatedResponse.getData()).orElse(Collections.emptyList());
         List<EmployeeProfileDTO> serviceNowResponse = content.stream()
                 .map(serviceNowMapper::toEmployeeProfileDTO)
                 .toList();
 
         return PaginatedResponse.<EmployeeProfileDTO>builder()
                 .data(serviceNowResponse)
-                .totalRecords(sapPaginatedResponse.getTotalRecords())
-                .skip(sapPaginatedResponse.getSkip())
-                .limit(sapPaginatedResponse.getLimit())
-                .hasNext(sapPaginatedResponse.isHasNext())
-                .nextSkip(sapPaginatedResponse.getNextSkip())
+                .totalRecords(paginatedResponse.getTotalRecords())
+                .skip(paginatedResponse.getSkip())
+                .limit(paginatedResponse.getLimit())
+                .hasNext(paginatedResponse.isHasNext())
+                .nextSkip(paginatedResponse.getNextSkip())
                 .build();
+    }
+
+    private String buildUrl(String baseUrl, String endpoint, String id) {
+        if (baseUrl == null) baseUrl = "";
+        if (endpoint == null) endpoint = "";
+        String normalizedBase = baseUrl.endsWith("/") ? baseUrl.substring(0, baseUrl.length() - 1) : baseUrl;
+        String normalizedEndpoint = endpoint.startsWith("/") ? endpoint : "/" + endpoint;
+        if (!normalizedEndpoint.endsWith("/")) {
+            normalizedEndpoint = normalizedEndpoint + "/";
+        }
+        return normalizedBase + normalizedEndpoint + id;
     }
 }
